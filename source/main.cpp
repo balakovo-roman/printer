0751d831416104ca554798e237d967db5fe8d5ce
Lebedev Roman <balakovo_roman@mail.ru>
2025-04-05 15:10:36 +0300

    Added printing integer value

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,32 @@
+
+#include <climits>
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+std::enable_if_t<std::is_integral_v<T>, void> print_ip(T value)
+{
+    for (std::size_t i = sizeof(T); i > 0; --i)
+    {
+        const auto byte =
+            static_cast<std::byte>((value >> ((i - 1) * CHAR_BIT)) & 0xFF);
+
+        std::cout << std::to_integer<unsigned>(byte);
+
+        if (i != 1)
+        {
+            std::cout << '.';
+        }
+    }
+
+    std::cout << '\n';
+}
+
 int main()
 {
-    return 0;
+    print_ip(int8_t{-1});
+    print_ip(uint16_t{0});
+    print_ip(int32_t{2130706433});
 }
